7e3f4481fbbc3e1bfa3f2ddafb58cc9330e510c6
nastex21 <nasax2000@gmail.com>
2020-10-16 16:34:12 -0500

    Added react-measure and also added responsivitity to the gallery

--- client/src/Components/English/Gallery.js
+++ client/src/Components/English/Gallery.js
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import Container from "react-bootstrap/Container";
 import axios from "axios";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import GalleryModule from "react-photo-gallery";
+import Measure from 'react-measure';
 
 function Gallery() {
   const [imageArray, setArray] = useState([]);
-
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const [width, setWidth] = useState(0);
+  
   useEffect(() => {
     var imageApi = "/api/get-images";
 
@@ -15,11 +21,57 @@ function Gallery() {
     });
   }, []);
 
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   console.log(imageArray);
   return (
-    <div>
-      <GalleryModule photos={imageArray} />
-    </div>
+    <Measure bounds onResize={(contentRect) => setWidth({ width: contentRect.bounds.width })}>
+      {   
+        ({ measureRef }) => {
+          if (width < 1 ){
+            return <div ref={measureRef}></div>;
+          }   
+          let columns = 1;
+          if (width >= 480){
+            columns = 2;
+          }   
+          if (width >= 1024){
+            columns = 3;
+          }   
+          if (width >= 1824){
+            columns = 4;
+          }   
+          return (
+          <Container>
+          <GalleryModule photos={imageArray} onClick={openLightbox} />
+          <ModalGateway>
+            {viewerIsOpen ? (
+              <Modal onClose={closeLightbox}>
+                <Carousel
+                  currentIndex={currentImage}
+                  views={imageArray.map(x => ({
+                    ...x,
+                    srcset: x.srcSet,
+                    caption: x.title
+                  }))}
+                />
+              </Modal>
+            ) : null}
+          </ModalGateway>
+        </Container>
+          )
+        }   
+      }   
+      </Measure>
+
   );
 }
 
