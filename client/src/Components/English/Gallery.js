c8b6c9d7a33a111d0f75b3129594248f763c7e0a
Tony <nasax2000@gmail.com>
2020-10-17 20:45:27 -0500

    Inifinite scroll works!
    - I want a window scroll instead of a div though.

--- client/src/Components/English/Gallery.js
+++ client/src/Components/English/Gallery.js
@@ -4,35 +4,60 @@ import axios from "axios";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import GalleryModule from "react-photo-gallery";
 import Measure from "react-measure";
-import InfiniteScroll from "react-infinite-scroll-component";
+import InfiniteScroll from "react-infinite-scroller";
 
 function Gallery() {
   const [imageArray, setArray] = useState([]);
+  const [images, setImages] = useState([]);
+  const [apiRequestLoaded, setRequest] = useState(false);
+
+  const [hasMoreItems, setHasMoreItems] = useState(true);
+
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
   const [width, setWidth] = useState(0);
-  //Dynamic Loading
-  const [images, setImages] = useState([]);
 
-  const loadMorePhotos = () => {
-    console.log('constantly being called')
-      setImages(
-        images.concat(imageArray.slice(images.length, images.length + 6))
-      );
-
-  };
-
-  useEffect(() => {
-    console.log('being called')
+  //get original set of photos
+  const getPhotos = () => {
     var imageApi = "/api/get-images";
 
     axios.get(imageApi).then((response, err) => {
       if (response) {
-        setArray([...response.data]);
+        setArray([...imageArray, ...response.data]);
+        setRequest(true)
       }
     });
-  }, []); 
+  };
 
+  //get next slice of photos
+  const loadMorePhotos = () => {
+    console.log("constantly being called");
+    console.log(
+      images.concat(imageArray.slice(images.length, images.length + 6))
+    );
+    setImages(
+      images.concat(imageArray.slice(images.length, images.length + 6))
+    );
+  };
+
+  useEffect(() => {
+    console.log("being called");
+    getPhotos();
+  }, []);
+
+  useEffect(() => {
+    if (images.length == 0) {
+      setImages(imageArray.slice(0, 6));
+    }
+  }, [imageArray]);
+
+  useEffect(() => {
+    if (images.length !== 0 && imageArray.length) {
+      if (images.length == imageArray.length) {
+        setHasMoreItems(false);
+      }
+    }
+  }, [images]);
 
   const openLightbox = useCallback((event, { photo, index }) => {
     setCurrentImage(index);
@@ -44,32 +69,34 @@ function Gallery() {
     setViewerIsOpen(false);
   };
 
+  const ImageDiv = () => {
+    var items = [];
+    images.map((track, i) => {
+      items.push(
+        <div className="track" key={i}>
+          <img src={track.src} alt="" width="150" height="150" />
+        </div>
+      );
+    });
+    return items;
+  };
+
+  const loader = <div className="loader">Loading ...</div>;
+
   console.log(imageArray);
+  console.log(images);
   return (
-          <Container>
-            <InfiniteScroll
-              dataLength={imageArray.length}
-              next={loadMorePhotos()}
-              hasMore={true}
-              loader={<h4>Loading...</h4>}
-            >
-              <GalleryModule photos={images} onClick={openLightbox} />
-              <ModalGateway>
-                {viewerIsOpen ? (
-                  <Modal onClose={closeLightbox}>
-                    <Carousel
-                      currentIndex={currentImage}
-                      views={imageArray.map((x) => ({
-                        ...x,
-                        srcset: x.srcSet,
-                        caption: x.title,
-                      }))}
-                    />
-                  </Modal>
-                ) : null}
-              </ModalGateway>
-            </InfiniteScroll>
-          </Container>
+   <Container style={{height:100 + 'vh', overflow: 'auto'}} >
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={apiRequestLoaded ? loadMorePhotos : null}
+        hasMore={hasMoreItems}
+        loader={loader}
+        useWindow={false}
+        >
+        <GalleryModule photos={images} onClick={openLightbox} />
+      </InfiniteScroll>
+    </Container>
   );
 }
 
