7032246e1384c1db12a665adeaee179a74a8c82c
nastex21 <nasax2000@gmail.com>
2020-10-19 19:43:37 -0500

    commit

--- client/src/Components/English/Gallery.js
+++ client/src/Components/English/Gallery.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import Container from "react-bootstrap/Container";
 import axios from "axios";
-import useInfiniteScroll from "../util/useInfiniteScroll";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import GalleryModule from "react-photo-gallery";
 import Measure from "react-measure";
@@ -10,14 +9,31 @@ function Gallery() {
   const [imageArray, setArray] = useState([]);
   const [images, setImages] = useState([]);
 
-  const [apiRequestLoaded, setRequest] = useState(false);
+  const imgReducer = (state, action) => {
+    switch (action.type) {
+      case 'STACK_IMAGES':
+        return { ...state, images: state.images.concat(imageArray.slice(images.length, images.length + 6)) }
+      case 'FETCHING_IMAGES':
+        return { ...state, fetching: action.fetching }
+      default:
+        return state;
+    }
+  }
+  const [imgData, imgDispatch] = useReducer(imgReducer,{ images:[], fetching: true});
+  // next code block goes here
 
-  const [hasMoreItems, setHasMoreItems] = useState(true);
-
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-  const [width, setWidth] = useState(0);
-  const [isFetching, setIsFetching] = useInfiniteScroll(loadMorePhotos);
+  var intObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      console.log(entry)
+      console.log(entry.isIntersecting) // returns true if the target intersects the root element
+    })
+  },
+  {
+    // default options
+  }
+);
+let target = document.querySelector('#root');
+intObserver.observe(target); // start observation
 
   //get original set of photos
   const getPhotos = () => {
@@ -39,7 +55,6 @@ function Gallery() {
       setImages(
         images.concat(imageArray.slice(images.length, images.length + 6))
       );
-      setIsFetching(false);
     }, 2000);
   }
 
@@ -64,26 +79,12 @@ function Gallery() {
     }
   }, [images]);
 
-  /*   {images.map((item, i)  => <img src={item.src}  key={i} style={{height: '1200px', width: '100px'}}/>)}
-   */
-
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
-
   const imageScroll = () => {
     console.log("called");
     return (
       <>
-        <GalleryModule photos={images} onClick={openLightbox} />
+        <GalleryModule photos={images} />
 
-        {isFetching && "Fetching more list items..."}
       </>
     );
   };
